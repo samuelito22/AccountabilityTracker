f3c64ea0a4c8a6c89e47802fb87f7c6a94a902f7
MichaelK999 <115144292+MichaelK999@users.noreply.github.com>
2024-06-28 13:59:56 +0100

    added registration

---
+++ client/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import LoginBox from "@/components/login-page/LoginBox";
+import SignUpBox from "@/components/login-page/SignUpBox";
+import Link from "next/link";
+
+export default function Login() {
+    return(
+        <>
+        <div className="flex  m-3 font-plex font-semibold">
+            <Link href="/">Back</Link>
+        </div>
+        <SignUpBox/>
+
+        </>
+    );
+}
+
+
